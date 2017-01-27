566db621f7fb24e458d49e30695d006932081d18
luixmartinv2 <luixmartinv2@gmail.com>
2017-01-27 17:53:21 +0100

    Coomit 11 a solo un paso

--- agenda telefonica/src/Cliente/Cliente.java
+++ agenda telefonica/src/Cliente/Cliente.java
@@ -8,11 +8,12 @@ import java.net.Socket;
 import java.util.Scanner;
 
 import servidor.Contacto;
+import servidor.Servidor;
 
 public class Cliente {
 	static Scanner sc = new Scanner(System.in);
 	static int m = '1';
-
+	
 	public static void main(String[] args) {
 		try {
 			System.out.println("Creando socket cliente");
@@ -22,27 +23,35 @@ public class Cliente {
 			clientSocket.connect(addr);
 			InputStream is = clientSocket.getInputStream();
 			OutputStream os = clientSocket.getOutputStream();
-			//  la acción    ///////////////////////////////////
+
+			// la acción ///////////////////////////////////
 			System.out.println("introduce el codigo de mensage: ");
 			System.out.println("1 para borrar, 2 para editar, 3 para mostrar, 4 para agregar contacto");
 			m = sc.nextInt();
-			//  el contacto  ///////////////////////////////////
-			String N = "";
+			//////////////////////////////////////////////////
+
+			// el contacto ///////////////////////////////////
+			String N1 = "";
+			byte[] N2 = new byte[20];
 			int T = 0;
 
 			System.out.println("Escribe el Nombre cullo numero quieres editar");
-			N = sc.next();
+			N1 = sc.next();
+			for (int i = 0; i < N2.length; i++) {
+				N2[0] = Byte.parseByte(N1);
+			}
+			os.write(N2);
+			Contacto.setId(N1);
 			System.out.println("Escribe el nuevo numero");
 			T = sc.nextInt();
-
-			Contacto.setId(N);
+			os.write(T);
 			Contacto.setTelf(T);
-			/////////////////////////////////////////////////////
+
+			////////////////////////////////////////////////
 			System.out.println("Enviando mensaje");
 			int num1 = m;
 			os.write(num1);
-			
-			
+
 			System.out.println("Mensaje enviado");
 			String mensaje = "";
 			is.read();
@@ -54,7 +63,5 @@ public class Cliente {
 			e.printStackTrace();
 		}
 	}
-	
-	
 
 }
--- agenda telefonica/src/servidor/Servidor.java
+++ agenda telefonica/src/servidor/Servidor.java
@@ -26,21 +26,40 @@ public class Servidor implements IntAgenda {
 			System.out.println("Conexión recibida");
 			InputStream is = newSocket.getInputStream();
 			OutputStream os = newSocket.getOutputStream();
-			String mensaje = "";
-			is.read();
-			System.out.println(mensaje.trim());
-			
-			if(mensaje=="1"){
-				
-				nuevoContactos(Contacto.getSting());
-			} else if(mensaje=="2"){
-				borrarContactos();
-			}else if(mensaje=="3"){
-				editarContactos();
-			}else if(mensaje=="4"){
-				mostrarContactos();
+			byte[] mensaje1 = new byte[20];
+			String mensaje2 = "";
+			int ac = 0;
+			try {
+				is.wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			is.read(mensaje1);
+			System.out.println(mensaje1.length);
+			mensaje2 = mensaje1.toString();
+			if (mensaje2.trim() == "1") {
+
+				ac = 1;
+			} else if (mensaje2.trim() == "2") {
+				ac = 2;
+			} else if (mensaje2.trim() == "3") {
+				ac = 3;
+			} else if (mensaje2.trim() == "4") {
+				ac = 4;
 			}
 			
+			//////aqui se dice que acción realizar
+			if (ac == 1) {
+				//borrarContactos(contacto);
+			} else if (ac == 2) {
+				//editarContactos(contacto);
+			} else if (ac == 3) {
+				//mostrarContactos(contacto);
+			} else if (ac == 4) {
+				//nuevoContactos(contacto);
+			}
+
 			System.out.println("Cerrando el nuevo socket");
 			newSocket.close();
 			System.out.println("Cerrando el socket servidor");
@@ -52,13 +71,13 @@ public class Servidor implements IntAgenda {
 
 	@Override
 	public void nuevoContactos(Contacto nuevoContacto) {
-		
+
 		ContactoRepository.crear(nuevoContacto);
 
 	}
 
 	@Override
-	public void borrarContactos(Contacto borrarContacto) {
+	public  void borrarContactos(Contacto borrarContacto) {
 
 		ContactoRepository.delete(borrarContacto);
 
